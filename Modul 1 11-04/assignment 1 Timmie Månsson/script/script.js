f4949845a0ee15fbe213a5bbeafbdaeb0494c084
TotallyNotTim1 <timmielolhype@gmail.com>
2024-11-05 07:14:07 +0100

    Add files via upload

---
+++ Modul 1 11-04/assignment 1 Timmie Månsson/script/script.js
@@ -0,0 +1,21 @@
+    document.title = "Assignment1"
+
+    
+
+    let foo 
+    let bar
+    foo = 0
+    bar = 0
+
+    console.log( bar )
+    console.log( foo )
+   
+    //alert("You Cant Divide Us!")
+    
+
+    
+    
+
+    
+    
+   
